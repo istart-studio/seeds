32323d20f02ab6384f9922934fbaae2ca2073dad
dongyan <dong-_-yan@163.com>
2018-05-09 15:33:22 +0800

    Expression： 增加操作符的结合性及处理逻辑，并优化代码

--- algorithm/src/main/java/studio/istart/algorithm/stack/Expression.java
+++ algorithm/src/main/java/studio/istart/algorithm/stack/Expression.java
@@ -25,8 +25,9 @@ public class Expression {
     }
 
     /**
+     * 构建表达式
      * @param expressionCharts 中缀表达式
-     * @return
+     * @return  Expression
      */
     public static Expression build(String expressionCharts) {
         Expression expression = new Expression();
@@ -34,11 +35,19 @@ public class Expression {
         return expression;
     }
 
+    /**
+     * 输出队列
+     */
     private Queue<Character> output = new ConcurrentLinkedQueue<>();
+    /**
+     * 操作符栈
+     */
     private Stack<OperatorSymbol> operatorStack = new Stack<>();
 
     /**
      * 中缀转后缀
+     * todo:现在只支持正整数，支持实数
+     * <p>
      * 调度场算法
      * 1.运算符具有优先级
      * 2.如果输入是运算符，则压入操作符堆栈
@@ -46,13 +55,13 @@ public class Expression {
      * 0.如果这个记号表示一个函数参数的分隔符（例如，一个半角逗号 , ）：
      * 0.>从栈当中不断地弹出操作符并且放入输出队列中去，直到栈顶部的元素为一个左括号为止。
      * 0.>如果一直没有遇到左括号，那么要么是分隔符放错了位置，要么是括号不匹配。
-     * todo:现在只支持正整数，支持实数
      * 1.如果这个记号表示一个操作符，记做o1，那么：
      * 1.>只要存在另一个记为o2的操作符位于栈的顶端，并且
      * 1.>如果o1是左结合性的并且它的运算符优先级要小于或者等于o2的优先级，或者
-     * 1.>todo:如果o1是右结合性的并且它的运算符优先级比o2的要低，那么
-     * 1.>>将o2从栈的顶端弹出并且放入输出队列中（循环直至以上条件不满足为止）；
-     * 1.>然后，将o1压入栈的顶端。
+     * 1.>如果o1是右结合性的并且它的运算符优先级比o2的优先级，
+     * 1.>>那么,将o2从栈的顶端弹出并且放入输出队列中（循环直至以上条件不满足为止）；
+     * 1.>最后，将o1压入栈的顶端。
+     *
      * @return 后缀表达式
      */
     public String toRPN() {
@@ -87,8 +96,9 @@ public class Expression {
     }
 
     /**
-     * 操作符设置
-     * @param symbolChar
+     * 操作符处理
+     *
+     * @param symbolChar 要处理的操作符
      */
     private void setOperatorSymbol(char symbolChar) {
         Optional<OperatorSymbol> operatorSymbolOptional = SYMBOLS.stream().filter(operatorSymbol -> operatorSymbol.element.charValue() == symbolChar).findAny();
@@ -113,42 +123,55 @@ public class Expression {
             operatorStack.push(operatorSymbol);
             return;
         }
-        //其他符号
-        OperatorSymbol queueSymbol = operatorStack.empty() ? null : operatorStack.peek();
-        //todo:^号为从右至左求值(右结合性),无优先级相等
-        if (operatorSymbol.element == '^' && operatorSymbol.priority == queueSymbol.priority) {
-            operatorStack.push(operatorSymbol);
-            return;
-        }
-        //优先级高于队列中的符号:入堆栈
-        if (queueSymbol == null || operatorSymbol.priority > queueSymbol.priority) {
-            operatorStack.push(operatorSymbol);
-            //优先级相同或等于:弹出堆栈加入输出队列
-        } else if (operatorSymbol.priority <= queueSymbol.priority) {
+        OperatorSymbol stackSymbol = operatorStack.empty() ? null : operatorStack.peek();
+        while (stackSymbol != null && getStackTop(operatorSymbol, stackSymbol)) {
             output.add(operatorStack.pop().element);
-            operatorStack.push(operatorSymbol);
+            stackSymbol = operatorStack.empty() ? null : operatorStack.peek();
         }
+        operatorStack.push(operatorSymbol);
     }
 
-    private static final List<OperatorSymbol> SYMBOLS = init();
+    /**
+     * 是否需要从操作符栈中弹出
+     * @param operatorSymbol    当前操作符
+     * @param stackSymbol 栈中第一个元素
+     * @return T:需要 F:不需要
+     */
+    private boolean getStackTop(OperatorSymbol operatorSymbol, OperatorSymbol stackSymbol) {
+        return (operatorSymbol.associativity == AssociativityEnum.LEFT && operatorSymbol.priority <= stackSymbol.priority)
+            || (operatorSymbol.associativity == AssociativityEnum.RIGHT && operatorSymbol.priority < stackSymbol.priority);
+    }
 
-    public static List<OperatorSymbol> init() {
+    /**
+     * 已知的操作符号
+     */
+    private static final List<OperatorSymbol> SYMBOLS = initSymbols();
+
+    /**
+     * 设定已知的操作符号的属性
+     * @return 已知的操作符号集合
+     */
+    public static List<OperatorSymbol> initSymbols() {
         List<OperatorSymbol> operatorSymbols = new ArrayList();
-        operatorSymbols.add(new OperatorSymbol('+', 1));
-        operatorSymbols.add(new OperatorSymbol('-', 1));
-        operatorSymbols.add(new OperatorSymbol('*', 2));
-        operatorSymbols.add(new OperatorSymbol('/', 2));
-        operatorSymbols.add(new OperatorSymbol('^', 3));
-        operatorSymbols.add(new OperatorSymbol(')', -1));
-        operatorSymbols.add(new OperatorSymbol('(', -1));
+        operatorSymbols.add(new OperatorSymbol('+', 1, AssociativityEnum.LEFT));
+        operatorSymbols.add(new OperatorSymbol('-', 1, AssociativityEnum.LEFT));
+        operatorSymbols.add(new OperatorSymbol('*', 2, AssociativityEnum.LEFT));
+        operatorSymbols.add(new OperatorSymbol('/', 2, AssociativityEnum.LEFT));
+        operatorSymbols.add(new OperatorSymbol('^', 3, AssociativityEnum.RIGHT));
+        operatorSymbols.add(new OperatorSymbol(')', -1, AssociativityEnum.NONE));
+        operatorSymbols.add(new OperatorSymbol('(', -1, AssociativityEnum.NONE));
         return operatorSymbols;
     }
 
+    /**
+     * 操作符号
+     */
     @Data
     @AllArgsConstructor
     public static class OperatorSymbol {
         private Character element;
         private int priority;
+        private AssociativityEnum associativity;
 
         @Override
         public boolean equals(Object o) {
@@ -173,5 +196,20 @@ public class Expression {
             result = 31 * result + getElement().hashCode();
             return result;
         }
+    }
+
+    /**
+     * 结合性
+     */
+    enum AssociativityEnum {
+        /**
+         * 右结合性
+         */
+        RIGHT,
+        /**
+         * 左结合性
+         */
+        LEFT,
+        NONE
     }
 }
